d453509d3ce5185df307030cd9c9ce10b26e483f
rudetm32 <jaruiz721818@gmail.com>
2023-03-29 01:32:21 -0600

    feature(src/routes/catRoutes):  created routes cat

---
+++ api/src/routes/catRoutes.ts
@@ -0,0 +1,24 @@
+import {Response, Request, Router, NextFunction} from 'express';
+import { Cat } from '../models/Cat';
+
+const router: Router= Router()
+
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  Cat.findAll()
+   .then((Cats) => {
+    res.send(Cats);
+   })
+   .catch((error) => next(error));
+ });
+
+ router.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const cat = req.body;
+  Cat.create(cat)
+   .then((createdCat) => {
+    res.send(createdCat);
+   })
+   .catch((error) => next(error));
+ });
+
+
+export default router;
