2a7bb85c220e304fe3359cf7318d0143b876e09d
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-29 14:16:01 -0300

    fix(index.ts): port to number

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,9 @@
 import {sequelize} from "./src/db"
 import app from './src/app';
 import config from './lib/config';
-
+const port = Number(config.port);
 sequelize
 .sync({force: true, logging: false})
-app.listen(config.port,'0.0.0.0',function () {
+app.listen(port,'0.0.0.0',function () {
  console.log(`App is listening on port ${config.port}!`);
 });
