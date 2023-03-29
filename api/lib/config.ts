8f5aa82cb1c995053baf5542869fbb4a6e2818f1
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-29 17:02:05 -0300

    feat(lib/config): deploy

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -3,16 +3,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
- dbUser: process.env.DB_USER || 'postgres',
- dbPassword: process.env.DB_PASSWORD || '123456',
- dbHost: process.env.DB_HOST || 'localhost',
- dbName: process.env.DB_NAME || 'railway',
- dbPort: process.env.DB_PORT || "000",
- dev: process.env.NODE_ENV !== 'production',
- port: process.env.API_PORT || "3001",
- host: process.env.API_host || 'localhost',
- cors: process.env.CORS || '*',
- dialect:process.env.DIALECT || "postgres"
+ dbUser: process.env.DB_USER ,
+ dbPassword: process.env.DB_PASSWORD ,
+ dbHost: process.env.DB_HOST ,
+ dbName: process.env.DB_NAME ,
+ dbPort: process.env.DB_PORT ,
+ dev: process.env.NODE_ENV ,
+ port: process.env.API_PORT ,
+ host: process.env.API_host  ,
+ cors: process.env.CORS  ,
+ dialect:process.env.DIALECT 
 };
 
 
