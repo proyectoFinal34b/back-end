237419305eb403c861fc94936209eed2018e9174
rudetm32 <jaruiz721818@gmail.com>
2023-03-29 01:28:30 -0600

    refactor(src/routes/userRoutes):  conectar base de datos con la ruta

--- api/src/routes/userRoutes.ts
+++ api/src/routes/userRoutes.ts
@@ -1,14 +1,24 @@
-import {Response, Request, Router} from 'express';
+import {Response, Request, Router, NextFunction} from 'express';
+import { User } from '../models/User';
 
 const router: Router= Router()
 
-router.get('/', (req: Request, res: Response) => {
- res.send('soy la ruta getUser!');
-});
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
+  User.findAll()
+   .then((users) => {
+    res.send(users);
+   })
+   .catch((error) => next(error));
+ });
 
-router.post('/', (req: Request, res: Response) => {
-    res.send('soy la ruta postUser!');
-   });
+ router.post('/', (req: Request, res: Response, next: NextFunction) => {
+  const user = req.body;
+  User.create(user)
+   .then((createdUser) => {
+    res.send(createdUser);
+   })
+   .catch((error) => next(error));
+ });
 
 
 export default router;
