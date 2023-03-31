ac80007b62718bbf94f1ad390f4bafb55d4053d2
rudetm32 <jaruiz721818@gmail.com>
2023-03-31 00:04:40 -0600

    feat(controllers/userControllers): add metodo put

--- api/src/controllers/userControllers.ts
+++ api/src/controllers/userControllers.ts
@@ -78,9 +78,34 @@ export const delUser= async (req: Request, res: Response, next: NextFunction)=>{
     }
 }
 
-export  const putUser = (req: Request, res: Response, next: NextFunction)=>{
-    res.send("ruta actualizar user")
-} 
+export const updateUser = (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+      
+      const { name, lastName, email, active, phoneNumber, image} = req.body;
+      User.findByPk(id)
+      .then((user) => {
+          if(user){
+              user.name = name || user.name;
+              user.lastName = lastName || user.lastName;
+              user.email = email || user.email;
+              user.active = active || user.active;
+              user.phoneNumber =phoneNumber ||user.phoneNumber;
+              user.image = image || user.image;
+
+            user.save()
+              .then((updated) => {
+                  res.status(200).send(updated);
+              });
+          } else {
+              res.status(404).send(`Usuario con id ${id} no encontrado`);
+          }
+      });
+  } catch (error) {
+      res.status(400).json(error);
+  }
+}
+
 
 
 
