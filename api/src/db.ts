bed535b1202b12bae4aae6567e80d85900ef1bcb
proyectoFinal34b <129087992+proyectoFinal34b@users.noreply.github.com>
2023-03-30 21:42:52 -0300

    Update db.ts

--- api/src/db.ts
+++ api/src/db.ts
@@ -16,7 +16,7 @@ export const sequelize = new Sequelize({
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    port: 7746,
+    port: 5593,
     models: [__dirname + '/models'],
   });
 
