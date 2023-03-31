f4e94aeb8cc28689dc09be8d5e0575bfa5ef1ab7
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-03-30 22:56:37 -0300

    Product has many Rating

--- api/src/controllers/productControllers.ts
+++ api/src/controllers/productControllers.ts
@@ -1,6 +1,7 @@
 import {Response, Request, NextFunction} from 'express';
 import { sequelize } from '../db';
 import { Product } from "../models/Product"
+import { Rating } from '../models/Rating';
 
 export const getProduct= function(req: Request, res: Response, next: NextFunction){
     Product.findAll({
@@ -21,3 +22,16 @@ export const postProduct= function(req: Request, res: Response, next: NextFuncti
     })
     .catch((error) => next(error));
 }
+
+
+export const postRating = (req: Request, res: Response, next: NextFunction) => {
+    const productId = req.params.id;
+    const ratingData = req.body;
+    ratingData.productId = productId;
+  
+    Rating.create(ratingData)
+      .then((createdRating) => {
+        res.send(createdRating);
+      })
+      .catch((error) => next(error));
+  };
