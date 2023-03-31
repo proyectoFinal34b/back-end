ef9c44474fd78ebf963e7879d408f277d6c01b0b
rudetm32 <jaruiz721818@gmail.com>
2023-03-31 12:58:30 -0600

    refactor(api/src/models/Cat.ts): add atributos state || cambio tipo de dato arrived(date)

--- api/src/models/Cat.ts
+++ api/src/models/Cat.ts
@@ -18,7 +18,8 @@ name!: string;
  
  @AllowNull(false)
  @Column
- sexo!: string;
+ gender!: string;
+ 
 
  @AllowNull(false)
  @Column
@@ -28,13 +29,17 @@ name!: string;
  @Column(DataType.ARRAY(DataType.STRING))
  image!:string[];
 
- @AllowNull(true)
+ @AllowNull(false)
  @Column({ defaultValue: true })
  status!:boolean;
 
+ @AllowNull(false)
+ @Column
+ state!:string;// establecer como opciones: adoptado || apadrinado || alberge
+
  @AllowNull(true)
  @Column
- arrived!:string
+ arrived!:Date
 
  @CreatedAt
  @Column
