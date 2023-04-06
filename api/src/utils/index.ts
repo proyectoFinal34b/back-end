183d0e2a01a009c2b17c01b47630e1684689811a
rudetm32 <jaruiz721818@gmail.com>
2023-04-06 09:32:31 -0600

    feat(src/utils/index.ts): modificar atributo descripcion

--- api/src/utils/index.ts
+++ api/src/utils/index.ts
@@ -15,7 +15,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"adoptado", arrived:"01-01-2021"},
+    status:true, state:"Adoptado", arrived:"01-01-2021"},
     
     {name:"Bolita", age:2, gender:"Hembra", 
     description:"💖Bolita 💖 ✨\n Esta gatita  está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -51,7 +51,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Pitts", age:3, gender:"macho", 
     description:"💖 Pitts 💖  ✨\n Este gatito está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -60,7 +60,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrino", arrived:"01-01-2021"},
     
     {name:"Kimy", age:2, gender:"Hembra", 
     description:"💖 Kimy 💖  ✨\n Esta gatita está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -78,7 +78,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Michu", age:4, gender:"Macho", 
     description:"💖 Michu 💖  ✨\n Este gatito está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -87,7 +87,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Lapiz", age:1, gender:"Macho", 
     description:"💖 Lapiz 💖  ✨\n Este gatito está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -96,7 +96,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Zulu", age:5, gender:"Macho", 
     description:"💖 Zulu 💖  ✨\n Este gatito está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -114,7 +114,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Adoptado", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Pepe", age:1, gender:"Macho",
     description:"💖 Pepe 💖  ✨\n Este gatito está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -132,7 +132,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Donacion", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Nefer", age:2, gender:"Hembra", 
     description:"💖 Nefer 💖  ✨\n Esta gatita está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
@@ -141,7 +141,7 @@ export const dbCat = [
         "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
         "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
         "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
-    status:true, state:"Adoptado", arrived:"01-01-2021"},
+    status:true, state:"Apadrinado", arrived:"01-01-2021"},
     
     {name:"Rubi", age:5, gender:"Hembra", 
     description:"💖 Rubi 💖  ✨\n Esta gatita está con nosotros hace casi dos años, nos alegramos mucho que por fin haya encontrado un hogar donde la quieran y cuiden como ella se merece!\n Y contarles que su hermanita también fue adoptada en Bastet en el año 2016 💝🙏🏼☺️\n Gracias a nuestros adoptantes por hacerlo posible! 💖",
