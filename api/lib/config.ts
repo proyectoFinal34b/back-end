1818b9896c51a6a2ea756e3ec9b3e22d0e59fdc8
proyectoFinal34b <129087992+proyectoFinal34b@users.noreply.github.com>
2023-04-06 01:38:51 -0300

    Update config.ts

--- api/lib/config.ts
+++ api/lib/config.ts
@@ -7,7 +7,7 @@ const config = {
  dbPassword: process.env.DB_PASSWORD ,
  dbHost: process.env.DB_HOST ,
  dbName: process.env.DB_NAME ,
- dbPort: 5432 ,
+ dbPort: process.env.DB_PORT ,
  dev: process.env.NODE_ENV ,
  port: process.env.API_PORT ,
  host: process.env.API_host  ,
