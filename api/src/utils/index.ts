90bd8118a3e2851b2ea48c768ae0e28e8428377c
rudetm32 <jaruiz721818@gmail.com>
2023-04-06 08:09:56 -0600

    feat api/src/utils/index.ts): Archivo Json

---
+++ api/src/utils/index.ts
@@ -0,0 +1,137 @@
+export const dbCat = [
+    {name:"Ramses", age:1, gender:"Macho", description:"Soy Ramses estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Adoptado", arrived:"01-01-2023"},
+    
+    {name:"America", age:1, gender:"Hembra", description:"Soy America estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"adoptado", arrived:"01-01-2023"},
+    
+    {name:"Bolita", age:2, gender:"Hembra", description:"Soy Bolita estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Alberge", arrived:"01-01-20233"},
+    
+    {name:"Algodon", age:2, gender:"macho", description:"Soy Algodon estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Apadrinado", arrived:"01-01-2023"},
+    
+    {name:"Rower", age:5, gender:"macho", description:"Soy Rower estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Albergue", arrived:"01-01-2023"},
+    
+    {name:"Coste", age:4, gender:"macho", description:"Soy Coste estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Pitts", age:3, gender:"macho", description:"Soy Pitts estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Kimy", age:2, gender:"Hembra", description:"Soy Kimy estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Albergue", arrived:"01-01-2023"},
+    
+    {name:"Canela", age:6, gender:"Hembra", description:"Soy Canela estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Michu", age:4, gender:"Macho", description:"Soy Micho estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Lapiz", age:1, gender:"Macho", description:"Soy Lapiz estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Zulu", age:5, gender:"Macho", description:"Soy Zulu estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Albergue", arrived:"01-01-2023"},
+    
+    {name:"Candy", age:3, gender:"Hembra", description:"Soy Candy estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Adoptado", arrived:"01-01-2023"},
+    
+    {name:"Pepe", age:1, gender:"Macho", description:"Soy Pepe estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Albergue", arrived:"01-01-2023"},
+    
+    {name:"Gomita", age:3, gender:"Hembra", description:"Soy Gomita estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Donacion", arrived:"01-01-2023"},
+    
+    {name:"Nefer", age:2, gender:"Hembra", description:"Soy Nefer estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Adoptado", arrived:"01-01-2023"},
+    
+    {name:"Rubi", age:5, gender:"Hembra", description:"Soy Rubi estoy buscascando que me adoptes o si no me puedes tener bastaria con donar algo para mi manutencion",
+    image:[
+        "https://farm2.staticflickr.com/1923/45579542552_10ab658104_o.jpg",
+        "https://farm2.staticflickr.com/1946/30689176647_85b85d05b6_o.jpg",
+        "https://parahamster.net/wp-content/uploads/2019/05/gato1.jpg",
+        "http://www.mundogatos.com/Uploads/mundogatos.com/ImagenesGrandes/fotos-de-gatitos-3-2-1.jpg"], 
+    status:true, state:"Adoptado", arrived:"01-01-2023"},
+]
